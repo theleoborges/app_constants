06ebb0169e1aa89371b3ab0769871cb76e5ca03e
wiseleyb <wiseleyb@rasputin.local>
2011-04-07 17:30:57 -0700

    Adding .rvmrc to .gitignore, adding ability to raise errors if env/constants are missing, updating README to reflect changes.  Updating tests

--- test/app_constants_spec.rb
+++ test/app_constants_spec.rb
@@ -17,7 +17,7 @@ describe "AppConstants" do
 
     AppConstants.public_url.should == "development.myawesomeapp.com"
 
-    expect { AppConstants.public_url << "trying something nasty" }.to raise_error(RuntimeError)
+    expect { AppConstants.public_url << "trying something nasty" }.to raise_error(TypeError)
   end
 
   it "should return nil for non-existing environments" do
@@ -36,6 +36,23 @@ describe "AppConstants" do
     AppConstants.max_upload_in_bytes.should == 1048576
   end
 
+  it "should raise a runtime error if raise_error_on_missing is true" do
+    AppConstants.config_path = "#{File.dirname(__FILE__)}/fixtures/constants.yml"
+    AppConstants.raise_error_on_missing = true
+    AppConstants.environment = "development"
+    AppConstants.load!
+    expect { AppConstants.private_url }.to raise_error(RuntimeError)
+    AppConstants.public_url.should == "development.myawesomeapp.com"
+    AppConstants.app_name == "Master of awesomeness"
+    AppConstants.environment = "production"
+    AppConstants.load!
+    expect { AppConstants.private_url }.to raise_error(RuntimeError)
+    AppConstants.public_url.should == "www.myawesomeapp.com"
+    AppConstants.app_name == "Master of awesomeness"
+    AppConstants.environment = "playpen"
+    expect { AppConstants.load! }.to raise_error(RuntimeError)
+  end
+  
   describe "#load!" do
 
     before(:each) do
